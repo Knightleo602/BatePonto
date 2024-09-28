88db67b875456202c2fde1866e40624f09b16553
KnightLeo <knightleo602@gmail.com>
2024-09-28 03:12:42 -0300

    #4 versao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.knightleo.bateponto"
         minSdk = 26
         targetSdk = 35
-        versionCode = 101
-        versionName = "v1.0.1"
+        versionCode = 102
+        versionName = "v1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
